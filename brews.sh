b9c17fdeb628845c86e939f257eb90524478de63
John Lynn <lynnjonathanp@gmail.com>
2025-08-02 22:26:28 -0400

    initialize repository

---
+++ brews.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+brew install stow
+brew install git
+brew install gh
+brew install zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
+brew install fzf bat
+brew install ripgrep fd
+brew install tmux neovim
+brew install mise
+
+brew install colima docker docker-completion
+brew install flyctl
+brew install --cask ghostty
+brew install --cask font-hack-nerd-font
