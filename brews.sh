9957bf4f224ee55a8509121ccdff1c8cd97c9f2b
John Lynn <lynnjonathanp@gmail.com>
2026-02-22 11:11:11 -0500

    clean up install script

--- brews.sh
+++ brews.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install stow
-brew install git
-brew install gh
-brew install lazygit
+brew install stow				# deploys these lovely dotfiles
+brew install git				# version control
+brew install gh					# github CLI
+brew install lazygit				# git TUI
 brew install zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting starship
-brew install fzf bat
+brew install fzf bat				# Better cat
 brew install ripgrep fd
 brew install tmux neovim
-brew install mise
+brew install mise				# 
 
-brew install colima docker docker-completion
-brew install flyctl
-brew install --cask ghostty
-brew install --cask font-hack-nerd-font
-brew install --cask vscodium
+brew install colima 				# Install container manager
+brew install docker 				# Install docker CLI
+brew install docker-completion			# Add auto-completion for the CLI
+
+brew install flyctl				# CLI for fly.io deployments
+
+brew install --cask ghostty			# Install a better terminal
+brew install --cask font-hack-nerd-font		# Font for terminal & vscode
+
+brew install --cask visual-studio-code		# IDE
+brew install neovim				# Next IDE
+
+brew install --cask obsidian			# Note Taking
+mkdir -p ~/vaults				# Directory for Obsidian Vaults
